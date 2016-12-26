595d3df52793b9dd3754faaef29010907f8267b2
Addy Osmani <addyosmani@gmail.com>
2016-12-25 16:16:05 -0800

    Add newly calibrated numbers

--- app/src/components/Hello.vue
+++ app/src/components/Hello.vue
@@ -77,22 +77,27 @@
 
 <script>
 const deviceConfig = [{
-  'name': 'Macbook Pro - Chrome',
-  'parse': 275.9,
-  'eval': 23.45,
+  'name': 'Macbook Pro - Chrome 55',
+  'parse': 247.844,
+  'eval': 13.65,
   'image': './static/macbook-pro.jpg'
 }, {
-  'name': 'Moto G4 - Chrome',
+  'name': 'Macbook Pro - Safari 10',
+  'parse': 69.914,
+  'eval': 173.93,
+  'image': './static/macbook-pro.jpg'
+}, {
+  'name': 'Moto G4 - Chrome 55',
   'parse': 1429,
   'eval': 73,
   'image': './static/motog4.jpg'
 }, {
-  'name': 'Moto G1 - Chrome',
-  'parse': 2714.32,
-  'eval': 135.824,
-  'image': './static/motog-gen1.jpg'
+  'name': 'Nexus 5X - Chrome 55',
+  'parse': 1038.965,
+  'eval': 55.339,
+  'image': './static/nexus5x.jpg'
 }, {
-  'name': 'Nexus 5 - Chrome',
+  'name': 'Nexus 5 - Chrome 55',
   'parse': 1834.46,
   'eval': 200.435,
   'image': './static/nexus5.jpg'
@@ -112,37 +117,47 @@ const deviceConfig = [{
   'eval': 71,
   'image': './static/iphone-5c.jpg'
 }, {
-  'name': 'PixelXL - Chrome',
+  'name': 'PixelXL - Chrome 55',
   'parse': 1114,
   'eval': 40,
   'image': './static/pixelxl.jpg'
 }, {
-  'name': 'Moto E - Chrome',
+  'name': 'Moto E - Chrome 55',
   'parse': 2806.21,
   'eval': 141.089,
   'image': './static/moto-e.jpg'
 }, {
-  'name': 'Galaxy S7 - Chrome',
+  'name': 'Galaxy S7 - Chrome 55',
   'parse': 956.16,
   'eval': 31.654,
   'image': './static/galaxy-s7.jpg'
 }, {
-  'name': 'Galaxy S5 - Chrome',
+  'name': 'Galaxy S6 - Chrome 55',
+  'parse': 1141.63,
+  'eval': 42.649,
+  'image': './static/galaxy-s6.jpg'
+}, {
+  'name': 'Galaxy S5 - Chrome 55',
   'parse': 2224.495,
   'eval': 75.909,
   'image': './static/galaxy-s5.jpg'
 }, {
-  'name': 'Nexus 7 - Chrome',
+  'name': 'Moto G1 - Chrome 55',
+  'parse': 2936.944,
+  'eval': 169.385,
+  'image': './static/motog-gen1.jpg'
+}, {
+  'name': 'Nexus 7 - Chrome 55',
   'parse': 3005.095,
   'eval': 115.724,
   'image': './static/nexus-7.jpg'
 }, {
-  'name': 'Thinkpad T430 - Chrome',
+  'name': 'Thinkpad T430 - Chrome 55',
   'parse': 290.51,
   'eval': 40.545,
   'image': './static/thinkpad-t430.jpg'
 }, {
-  'name': 'Thinkpad T430 - Firefox',
+  'name': 'Thinkpad T430 - Firefox 50',
   'parse': 165.205,
   'eval': 7.634,
   'image': './static/thinkpad-t430.jpg'
@@ -152,7 +167,7 @@ const deviceConfig = [{
   'eval': 1955.314,
   'image': './static/thinkpad-t430.jpg'
 }, {
-  'name': 'Android One Airtel 3G',
+  'name': 'Android One Airtel 3G - Chrome 55',
   'parse': 3092.525,
   'eval': 140.824,
   'image': './static/android-one.jpg'
