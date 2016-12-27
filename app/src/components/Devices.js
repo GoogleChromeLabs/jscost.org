fafb512b895a31a0df97c7fcc1979b7931748067
Addy Osmani <addyosmani@gmail.com>
2016-12-26 18:35:30 -0800

    Add more devices, device config.

--- app/src/components/Devices.js
+++ app/src/components/Devices.js
@@ -49,12 +49,12 @@ export default [{
   'eval': 141.089,
   'image': './static/moto-e.jpg'
 }, {
-  'name': 'Galaxy S7 - Chrome 55',
+  'name': 'Galaxy S7 Edge - Chrome 55',
   'parse': 956.16,
   'eval': 31.654,
   'image': './static/galaxy-s7.jpg'
 }, {
-  'name': 'Galaxy S6 - Chrome 55',
+  'name': 'Galaxy S6 Edge - Chrome 55',
   'parse': 1141.63,
   'eval': 42.649,
   'image': './static/galaxy-s6.jpg'
@@ -89,8 +89,90 @@ export default [{
   'eval': 1955.314,
   'image': './static/thinkpad-t430.jpg'
 }, {
-  'name': 'Android One Airtel 3G - Chrome 55',
+  'name': 'Android One Airtel - Chrome 55',
   'parse': 3092.525,
   'eval': 140.824,
   'image': './static/android-one.jpg'
+}, {
+  'name': 'Google Nexus 6P - Chrome 55',
+  'parse': 1273,
+  'eval': 612,
+  'image': './static/nexus-6p.jpg'
+}, {
+  'name': 'Samsung Galaxy Note II - Chrome 55',
+  'parse': 6051,
+  'eval': 1916,
+  'image': './static/samsung-galaxy-note-ii.jpg'
+},
+{
+  'name': 'Motorola XT1097 Moto X (2nd Gen) - Chrome 55',
+  'parse': 2314,
+  'eval': 427,
+  'image': './static/moto-x-1097-2nd-gen.jpg'
+},
+{
+  'name': 'Motorola XT1052 Moto X - Chrome 55',
+  'parse': 2828,
+  'eval': 149,
+  'image': './static/moto-x-1052.jpg'
+},
+{
+  'name': 'Sony D5803 Xperia Z3 Compact (WebView)',
+  'parse': 4677,
+  'eval': 140,
+  'image': './static/sony-xperia-z3.jpg'
+},
+{
+  'name': 'SonyEricsson C6603 Xperia Z (WebView)',
+  'parse': 3745,
+  'eval': 157,
+  'image': './static/sony-xperia-z3.jpg'
+},
+{
+  'name': 'Nexus 4 - Chrome 55',
+  'parse': 4663,
+  'eval': 382,
+  'image': './static/nexus4.jpg'
+},
+{
+  'name': 'Huawei Ascend Mate 7 - Chrome 55',
+  'parse': 3784,
+  'eval': 187,
+  'image': './static/ascend-mate-7.jpg'
+},
+{
+  'name': 'Samsung Galaxy Note 4 - Chrome 55',
+  'parse': 2473,
+  'eval': 92,
+  'image': './static/galaxy-note-4.jpg'
+},
+{
+  'name': 'Motorola MotoG3 - Chrome',
+  'parse': 1761,
+  'eval': 205,
+  'image': './static/moto-g3.jpg'
+},
+{
+  'name': 'LG G4 - Chrome 55',
+  'parse': 2245,
+  'eval': 211,
+  'image': './static/lh-h815-g4.jpg'
+},
+{
+  'name': 'Lenovo A6000 - Chrome',
+  'parse': 2161,
+  'eval': 118,
+  'image': './static/lenovo-a6000.jpg'
+},
+{
+  'name': 'Huawei P9 Plus - Chrome',
+  'parse': 1511,
+  'eval': 438,
+  'image': './static/huawei-p9.jpg'
+},
+{
+  'name': 'Samsung Galaxy A3 - Chrome',
+  'parse': 2326,
+  'eval': 291,
+  'image': './static/galaxy-a3.jpg'
 }]
