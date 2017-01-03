dae87bb346663e4aba96418b6351165c81a38a71
Addy Osmani <addyosmani@gmail.com>
2017-01-03 00:22:38 -0800

    (component) Timeline legend

---
+++ app/src/components/TimelineLegend.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span class='timeline-aggregated-legend-value'>{{value}}</span>
+    <span class='timeline-aggregated-legend-swatch' :style='getBackgroundColor'></span>
+    <span class='timeline-aggregated-legend-title'>{{title}}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: ['value', 'color', 'title'],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    getBackgroundColor () {
+      return 'background-color: ' + this.color
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
