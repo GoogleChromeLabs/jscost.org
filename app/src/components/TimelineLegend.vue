624657f4588a1a0164bc105db106118ba484620a
Addy Osmani <addyosmani@gmail.com>
2017-01-04 18:56:30 -0800

    (comments) TimelineLegend

--- app/src/components/TimelineLegend.vue
+++ app/src/components/TimelineLegend.vue
@@ -1,4 +1,19 @@
 <template>
+  <!--
+    TimelineLegend provides a convenience component for displaying numeric
+    data with a colorful swatch and title. It is similar to the UI found in the 
+    Chrome Developer Tools Timeline/Performance panel. 
+
+    It accepts three props: `value`, `color` and `title` that can be used 
+    as follows:
+
+    <timeline-legend
+       value="2.34s"
+       color="rgb(243, 210, 124)"
+       title="Total Scripting"></timeline-legend>
+
+    You are responsible for supplying your own numeric units for values. 
+    -->
   <div>
     <span class='timeline-aggregated-legend-value'>{{value}}</span>
     <span class='timeline-aggregated-legend-swatch' :style='getBackgroundColor'></span>
