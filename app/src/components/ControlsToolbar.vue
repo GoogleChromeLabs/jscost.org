89541b2618efa607accb95431edc9be5b630829b
Addy Osmani <addyosmani@gmail.com>
2017-02-12 12:32:09 -0800

    Address a11y focus issue

--- app/src/components/ControlsToolbar.vue
+++ app/src/components/ControlsToolbar.vue
@@ -196,30 +196,29 @@ label {
 }
 
 .mdl-button {
-    background: 0 0;
-    border: none;
-    border-radius: 2px;
-    color: #000;
-    position: relative;
-    /*height: 36px;*/
-    margin: 0;
-    min-width: 64px;
-    /*padding: 0 16px;*/
-    display: inline-block;
-    font-family: "Roboto","Helvetica","Arial",sans-serif;
-    font-size: 14px;
-    font-weight: 500;
-    text-transform: uppercase;
-    letter-spacing: 0;
-    overflow: hidden;
-    will-change: box-shadow;
-    transition: box-shadow .2s cubic-bezier(.4,0,1,1),background-color .2s cubic-bezier(.4,0,.2,1),color .2s cubic-bezier(.4,0,.2,1);
-    outline: none;
-    cursor: pointer;
-    text-decoration: none;
-    text-align: center;
-    /*line-height: 36px;*/
-    vertical-align: middle;
+  background: 0 0;
+  border: none;
+  border-radius: 2px;
+  color: #000;
+  position: relative;
+  margin: 0;
+  min-width: 64px;
+  /*padding: 0 16px;*/
+  display: inline-block;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 0;
+  overflow: hidden;
+  will-change: box-shadow;
+  transition: box-shadow .2s cubic-bezier(.4,0,1,1),background-color .2s cubic-bezier(.4,0,.2,1),color .2s cubic-bezier(.4,0,.2,1);
+  /*outline: none;*/
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+  /*line-height: 36px;*/
+  vertical-align: middle;
 }
 
 .mdl-button--raised.mdl-button--colored {
